adf215943969ab68e208a5933cdbdd350cd68ecf
Yami Odymel <yamiodymel@gmail.com>
2017-03-13 05:17:50 +0800

    sd/lb 現在支援 Round Robin 了

--- sd/lb/lb.go
+++ sd/lb/lb.go
@@ -4,37 +4,44 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/go-svc/svc/sd/consul"
 	"github.com/hashicorp/consul/api"
 	"golang.org/x/net/context"
 
 	"google.golang.org/grpc"
 )
 
-type BalancerOption struct {
+type ConsulOption struct {
 	ServiceName string
-	Consul      *api.Client
+	Consul      consul.Client
 	Tag         string
 }
 
-// Balancer 是一個負載平衡器。
-type Balancer struct {
-	Option    BalancerOption
-	Addresses []grpc.Address
+// ConsulBalancer 是一個負載平衡器。
+type ConsulBalancer struct {
+	ServiceName string
+	Tag         string
+	Consul      consul.Client
+	lastAddress int
+	Addresses   []grpc.Address
 }
 
-// NewBalancer 會建立新的負載平衡器。
-func NewBalancer(opt BalancerOption) grpc.Balancer {
-
-	return &Balancer{Option: opt}
+// NewBalancer 會依照接收的選項型態來建立新的指定形態負載平衡器。
+func NewBalancer(opt interface{}) grpc.Balancer {
+	switch opt := opt.(type) {
+	case ConsulOption:
+		return &ConsulBalancer{ServiceName: opt.ServiceName, Consul: opt.Consul, Tag: opt.Tag}
+	}
+	return nil
 }
 
 // Start 是用來初始化負載平衡器的函式。這個函式會在 gRPC 進行 Dial 時呼叫。
-func (b Balancer) Start(target string, config grpc.BalancerConfig) error {
+func (b ConsulBalancer) Start(target string, config grpc.BalancerConfig) error {
 
 	return nil
 }
 
-func (b Balancer) Up(addr grpc.Address) (down func(error)) {
+func (b ConsulBalancer) Up(addr grpc.Address) (down func(error)) {
 
 	down = func(err error) {
 
@@ -48,12 +55,21 @@ func (b Balancer) Up(addr grpc.Address) (down func(error)) {
 // 這個函式可以用來收集該次 RPC 的狀態到負載平衡器做為統計。
 //
 // 當負載平衡器無法從錯誤中回復時應該回傳 err 錯誤，如果回傳了錯誤，gRPC 會將該次的 RPC 請求標記為失敗。
-func (b Balancer) Get(ctx context.Context, opts grpc.BalancerGetOptions) (addr grpc.Address, put func(), err error) {
-	fmt.Println(b.Addresses)
+func (b *ConsulBalancer) Get(ctx context.Context, opts grpc.BalancerGetOptions) (addr grpc.Address, put func(), err error) {
 	if len(b.Addresses) == 0 {
 		addr = grpc.Address{}
 	} else {
-		addr = b.Addresses[0]
+		fmt.Println(b.Addresses)
+		if len(b.Addresses)-1 > b.lastAddress {
+			addr = b.Addresses[b.lastAddress+1]
+			fmt.Println(addr)
+			b.lastAddress++
+		} else {
+			addr = b.Addresses[0]
+			b.lastAddress = 0
+			fmt.Println(addr)
+		}
+
 	}
 	put = func() {
 
@@ -62,15 +78,17 @@ func (b Balancer) Get(ctx context.Context, opts grpc.BalancerGetOptions) (addr g
 	return
 }
 
-func (b *Balancer) Notify() <-chan []grpc.Address {
+func (b *ConsulBalancer) Notify() <-chan []grpc.Address {
 	ch := make(chan []grpc.Address)
 
 	go func() {
 		for {
+			// 每隔一段時間就從服務探索中心裡取得可用的指定類別服務。
 			<-time.After(3 * time.Second)
 
-			services, _, _ := b.Option.Consul.Catalog().Service(b.Option.ServiceName, b.Option.Tag, &api.QueryOptions{})
-			//fmt.Println(services)
+			// 從服務探索中心裡取得所有相關的服務。
+			services, _, _ := b.Consul.Client().Catalog().Service(b.ServiceName, b.Tag, &api.QueryOptions{})
+			// 如果服務探索中心裡沒有需要的服務，則略過此次搜尋，直接進行下一輪搜尋。
 			if len(services) == 0 {
 				continue
 			}
@@ -82,9 +100,7 @@ func (b *Balancer) Notify() <-chan []grpc.Address {
 					Addr: fmt.Sprintf("%s:%d", svc.Address, svc.ServicePort),
 				})
 			}
-			//[]grpc.Address{
-			//	{Addr: "localhost:50050"},
-			//}
+
 			b.Addresses = address
 
 			ch <- b.Addresses
@@ -95,7 +111,7 @@ func (b *Balancer) Notify() <-chan []grpc.Address {
 }
 
 // Close 會關閉負載平衡器。
-func (b Balancer) Close() error {
+func (b ConsulBalancer) Close() error {
 
 	return nil
 }
