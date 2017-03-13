19c28f5596f2d7a666c4082ab48da26511d9acc9
Yami Odymel <yamiodymel@gmail.com>
2017-03-13 19:04:41 +0800

    lb 現在更乾淨了，新增 pick

--- sd/lb/lb.go
+++ sd/lb/lb.go
@@ -1,125 +1,52 @@
 package lb
 
 import (
-	"fmt"
 	"math/rand"
 	"time"
-
-	"github.com/go-svc/svc/sd/consul"
-	"github.com/hashicorp/consul/api"
-	"golang.org/x/net/context"
 
 	"google.golang.org/grpc"
 )
 
 var (
+	// RoundRobin 是輪詢模式，負載平衡器每次都會按照順序逐一採用不同的伺服器，確保每個伺服器都會被使用到。
 	RoundRobin = 0
-	Random     = 1
+	// Random 是隨機模式，負載平衡器每次都會隨機挑選一個伺服器使用。
+	Random = 1
 )
-
-type ConsulOption struct {
-	ServiceName string
-	Mode        int
-	Consul      consul.Client
-	Tag         string
-}
-
-// ConsulBalancer 是一個負載平衡器。
-type ConsulBalancer struct {
-	Option      ConsulOption
-	lastAddress int
-	Addresses   []grpc.Address
-}
 
 // NewBalancer 會依照接收的選項型態來建立新的指定形態負載平衡器。
 func NewBalancer(opt interface{}) grpc.Balancer {
 	switch opt := opt.(type) {
+	// 接收到 ConsulOption 就建立一個 Consul 負載平衡器。
 	case ConsulOption:
 		return &ConsulBalancer{Option: opt}
+	// 接收到 FixedOption 就建立一個固定的負載平衡器。
+	case FixedOption:
+		return &FixedBalancer{Option: opt}
 	}
 	return nil
 }
 
-// Start 是用來初始化負載平衡器的函式。這個函式會在 gRPC 進行 Dial 時呼叫。
-func (b ConsulBalancer) Start(target string, config grpc.BalancerConfig) error {
-
-	return nil
-}
-
-func (b ConsulBalancer) Up(addr grpc.Address) (down func(error)) {
-
-	down = func(err error) {
-
+// pick 會依照傳入的模式來在傳入的實例群中挑選出一個伺服器，並將其與新的最後索引一同回傳。
+func pick(mode int, addrs []grpc.Address, last int) (grpc.Address, int) {
+	// 如果實例群是空的，就回傳空的地址。
+	if len(addrs) == 0 {
+		return grpc.Address{}, 0
 	}
-	return
-}
 
-// Get 會基於 ctx 內容來回傳一個 gRPC 伺服器的地址。
-//
-// put 函式會在 RPC 完成或是失敗的時候呼叫。
-// 這個函式可以用來收集該次 RPC 的狀態到負載平衡器做為統計。
-//
-// 當負載平衡器無法從錯誤中回復時應該回傳 err 錯誤，如果回傳了錯誤，gRPC 會將該次的 RPC 請求標記為失敗。
-func (b *ConsulBalancer) Get(ctx context.Context, opts grpc.BalancerGetOptions) (addr grpc.Address, put func(), err error) {
-	if len(b.Addresses) == 0 {
-		addr = grpc.Address{}
-	} else {
-		if b.Option.Mode == RoundRobin {
-			if len(b.Addresses)-1 > b.lastAddress {
-				addr = b.Addresses[b.lastAddress+1]
-				b.lastAddress++
-			} else {
-				addr = b.Addresses[0]
-				b.lastAddress = 0
-			}
-		} else {
-			rand.Seed(time.Now().Unix())
-			addr = b.Addresses[rand.Intn(len(b.Addresses))]
-			fmt.Println(addr)
+	// 依照指定的模式來在實力群內進行挑選。
+	switch mode {
+	// 輪詢模式。
+	case RoundRobin:
+		// 如果實例群中還有下一個可供使用，那麼就選擇下一個。
+		if len(addrs)-1 > last {
+			return addrs[last+1], last + 1
 		}
-
+		// 否則從頭來過。
+		return addrs[0], 0
+	// 隨機模式。
+	default:
+		rand.Seed(time.Now().Unix())
+		return addrs[rand.Intn(len(addrs))], 0
 	}
-	put = func() {
-
-	}
-	err = nil
-	return
-}
-
-func (b *ConsulBalancer) Notify() <-chan []grpc.Address {
-	ch := make(chan []grpc.Address)
-
-	go func() {
-		for {
-			// 每隔一段時間就從服務探索中心裡取得可用的指定類別服務。
-			<-time.After(3 * time.Second)
-
-			// 從服務探索中心裡取得所有相關的服務。
-			services, _, _ := b.Option.Consul.Client().Catalog().Service(b.Option.ServiceName, b.Option.Tag, &api.QueryOptions{})
-			// 如果服務探索中心裡沒有需要的服務，則略過此次搜尋，直接進行下一輪搜尋。
-			if len(services) == 0 {
-				continue
-			}
-
-			var address []grpc.Address
-
-			for _, svc := range services {
-				address = append(address, grpc.Address{
-					Addr: fmt.Sprintf("%s:%d", svc.Address, svc.ServicePort),
-				})
-			}
-
-			b.Addresses = address
-
-			ch <- b.Addresses
-		}
-	}()
-
-	return ch
-}
-
-// Close 會關閉負載平衡器。
-func (b ConsulBalancer) Close() error {
-
-	return nil
 }
