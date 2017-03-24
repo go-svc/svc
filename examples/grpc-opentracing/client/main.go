8032120b5fa4f36f4bf8d5fa0480f4fae37aefc0
Yami Odymel <yamiodymel@gmail.com>
2017-03-25 02:28:36 +0800

    嘗試新增 OpenTracing 範例

---
+++ examples/grpc-opentracing/client/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/go-svc/svc/examples/grpc-fixed-lb/pb"
+	"github.com/go-svc/svc/tracing/opentracing"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	tracer, err := opentracing.NewTracer(opentracing.Tracer{
+		Collector: opentracing.Collector{
+			URL: "http://localhost:9411/api/v1/spans",
+		},
+		Recorder: opentracing.Recorder{
+			Debug:       false,
+			Host:        "127.0.0.1:0",
+			ServiceName: "Client",
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// 連線到遠端 gRPC 伺服器。
+
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithUnaryInterceptor(tracer.ClientInterceptor()))
+	if err != nil {
+		log.Fatalf("連線失敗：%v", err)
+	}
+	defer conn.Close()
+
+	// 建立新的 Todo 客戶端，所以等一下就能夠使用 Todo 的所有方法。
+	c := pb.NewTodoClient(conn)
+
+	// 傳送新請求到遠端 gRPC 伺服器 Todo 中，並呼叫 Add 函式，新增一個工作記事。
+	r, err := c.Add(context.Background(), &pb.Task{Title: "測試", Content: "這是測試的內容。"})
+	if err != nil {
+		log.Fatalf("無法執行 Add 函式：%v", err)
+	}
+
+	log.Printf("Add 結果：%v, %v", r.Title, r.Content)
+
+	// 傳送新請求到遠端 gRPC 伺服器 Todo 中，並呼叫 List 函式，列出所有在資料庫內的工作記事。
+	tasks, err := c.List(context.Background(), &pb.Void{})
+	if err != nil {
+		log.Fatalf("無法執行 List 函式：%v", err)
+	}
+	t, _ := json.Marshal(tasks)
+	log.Printf("List 結果：%v", string(t))
+
+	time.Sleep(2 * time.Second)
+
+}
---
+++ examples/grpc-opentracing/database/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+
+	"golang.org/x/net/context"
+
+	"github.com/go-svc/svc/examples/grpc-fixed-lb/pb"
+	"github.com/go-svc/svc/tracing/opentracing"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+// port 是指定部署的埠口。
+var port string
+
+// server 建構體會實作 Todo 的 gRPC 伺服器。
+type server struct {
+	db     *gorm.DB
+	tracer *opentracing.Tracer
+}
+
+// Add 會插入接收到的資料到本地資料庫。
+func (s *server) Add(ctx context.Context, in *pb.Task) (*pb.Task, error) {
+	// 在資料庫內插入剛才接收到的工作記事資料。
+	s.db.Create(in)
+	return in, nil
+}
+
+// List 會回傳本地資料庫中所有的工作記事。
+func (s *server) List(ctx context.Context, in *pb.Void) (*pb.Tasks, error) {
+	var tasks []*pb.Task
+	// 取得資料庫內 `tasks` 資料表的所有紀錄。
+	s.db.Find(&tasks)
+	return &pb.Tasks{Tasks: tasks}, nil
+}
+
+// newConnection 會建立並回傳一個新的資料庫連線。
+func newConnection() *gorm.DB {
+	db, _ := gorm.Open("sqlite3", "/tmp/gorm.db")
+	// 初始化資料表格
+	db.DropTableIfExists(&pb.Task{})
+	db.CreateTable(&pb.Task{})
+	return db
+}
+
+func main() {
+	// 取得部署的埠口，如果以 `go run` 執行，那麼埠口就是 `os.Args[1]` 而不是 `os.Args[0]`。
+	port = os.Args[0]
+	if len(os.Args) == 2 {
+		port = os.Args[1]
+	}
+
+	// 監聽指定埠口，這樣服務才能在該埠口執行。
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("無法監聽該埠口：%v", err)
+	}
+
+	tracer, err := opentracing.NewTracer(opentracing.Tracer{
+		Collector: opentracing.Collector{
+			URL: "http://localhost:9411/api/v1/spans",
+		},
+		Recorder: opentracing.Recorder{
+			Debug:       false,
+			Host:        "127.0.0.1:" + port,
+			ServiceName: "Database",
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// 建立新 gRPC 伺服器並註冊 Todo 服務。
+	s := grpc.NewServer(grpc.UnaryInterceptor(tracer.ServerInterceptor()))
+	pb.RegisterTodoServer(s, &server{
+		// 建立連線到資料庫伺服器，所以稍後才能在本地伺服器中呼叫和資料庫相關的功能。
+		db: newConnection(),
+		//
+		tracer: tracer,
+	})
+
+	// 在 gRPC 伺服器上註冊反射服務。
+	reflection.Register(s)
+	// 開始在指定埠口中服務。
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("無法提供服務：%v", err)
+	}
+}
